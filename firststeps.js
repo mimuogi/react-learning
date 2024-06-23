37b2afec93f412110f1def5017867d1dbd9bd61e
mimuogi <mimuogi@gmail.com>
2024-06-24 01:19:56 +0200

    Add linter in main dir

--- firststeps.js
+++ firststeps.js
@@ -8,18 +8,19 @@ const appDomElement = document.getElementById('app')
 const root = ReactDOM.createRoot(appDomElement)
 
 const button = React.createElement(
-        'button',         // Elemento HTML que React envuelve
-        {"data-id": 123}, // Objeto con las propiedades para el HTML
-        'Me gusta')       // Contenido dentro de la etiqueta HTML
+  'button', // Elemento HTML que React envuelve
+  { 'data-id': 123 }, // Objeto con las propiedades para el HTML
+  'Me gusta') // Contenido dentro de la etiqueta HTML
 
 const button2 = React.createElement('button',{"data-id": 123},'Me gusta')       
 const button3 = React.createElement('button',{"data-id": 123}, 'Me gusta')
 
-//Reac puede encapsular un conjunto de cosas en una etiqueta como div por ejemplo
-//const misCosas = React.createElement('div', null, [button, button2, button3])
+// Reac puede encapsular un conjunto de cosas en una etiqueta como div por ejemplo
+// const misCosas = React.createElement('div', null, [button, button2, button3])
 // Para evitar dividitis, usa un React.Fragment así: (los fragments no añaden una etiqueta HTML adicional)
 
-const firstFragment = React.createElement(React.Fragment ,{"data-id": 123}, 'Me gusta')
+// eslint-disable-next-line no-unused-vars
+const firstFragment = React.createElement(React.Fragment , { "data-id": 123}, 'Me gusta')
 
 root.render(button)
 
